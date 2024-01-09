aaaab36e1866f6d710dff2e654fa1dc66364cba3
lutzgo <lutz0go@gmail.com>
2024-01-09 14:50:33 +0100

    added users/lgo

---
+++ users/lgo/default.nix
@@ -0,0 +1,50 @@
+{ config, lib, pkgs, ... }:
+let
+  ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+in
+  with lib;
+{
+  options = {
+    host.user.lgo = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Enable lgo";
+      };
+    };
+  };
+
+  config = mkIf config.host.user.lgo.enable {
+    users.users.lgo = {
+      isNormalUser = true;
+      shell = pkgs.bashInteractive;
+      uid = 2324;
+      group = "users" ;
+      extraGroups = [
+        "wheel"
+        "video"
+        "audio"
+      ] ++ ifTheyExist [
+        "adbusers"
+        "deluge"
+        "docker"
+        "git"
+        "input"
+        "libvirtd"
+        "lp"
+        "mysql"
+        "network"
+        "podman"
+      ];
+
+      openssh.authorizedKeys.keys = [ (builtins.readFile ./ssh.pub) ];
+      hashedPasswordFile = mkDefault config.sops.secrets.lgo-password.path;
+      packages = [ pkgs.home-manager ];
+    };
+
+    sops.secrets.lgo-password = {
+      sopsFile = mkDefault ../secrets.yaml;
+      neededForUsers = mkDefault true;
+    };
+  };
+}
